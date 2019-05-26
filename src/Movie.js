6e8b6c48d745770075d3ec4a8bb108c3e63d818f
ahnoldmoon <munnt72@hotmail.com>
2019-05-26 17:12:24 +0900

    #2-4. Validating Props with PropTypes : Complete

--- src/Movie.js
+++ src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         // console.log(this.props.title);
         // console.log(this.props);
@@ -15,6 +22,11 @@ class Movie extends Component{
 }
 
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
         console.log(this.props);
         return(
