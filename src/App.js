d5f5143e6cc2ca06e928ec261f771deb4ff9292a
ahnoldmoon <munnt72@hotmail.com>
2019-05-26 16:53:33 +0900

    #2-3. Lists with .map : Complete

--- src/App.js
+++ src/App.js
@@ -10,29 +10,46 @@ import Movie from './Movie';
 //   );
 // }
 
-const movieTitles = [
-  "Matrix",
-  "Full Metal Jacket",
-  "Oldboy",
-  "Star Wars"
-]
+// const movieTitles = [
+//   "Matrix",
+//   "Full Metal Jacket",
+//   "Oldboy",
+//   "Star Wars"
+// ]
 
-const movieImages = [
-  "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
-  "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg",
-  "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
-  "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
+// const movieImages = [
+//   "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg",
+//   "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg",
+//   "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg",
+//   "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
+// ]
+
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
+  }
 ]
 
 class App extends Component{
   render(){
     return (
       <div className="App">
-        hello!!
-        <Movie title={movieTitles[0]} poster={movieImages[0]} />
-        <Movie title={movieTitles[1]} poster={movieImages[1]} />
-        <Movie title={movieTitles[2]} poster={movieImages[2]} />
-        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster} />
+        })}
       </div>
     );
   }
