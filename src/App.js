420a7eaacf66b7cb53a013ced8106165ac15d082
ahnoldmoon <munnt72@hotmail.com>
2019-05-28 07:09:33 +0900

    #4-2. Practicing this.setState() : Complete

--- src/App.js
+++ src/App.js
@@ -24,24 +24,7 @@ import Movie from './Movie';
 //   "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
 // ]
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
-  }
-]
+
 
 class App extends Component{
 
@@ -54,15 +37,44 @@ class App extends Component{
   // }
 
   state = {
-    greeting: 'Hello'
+    greeting: 'Hello',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
+      }
+    ]
   }
 
   componentDidMount(){
+    // setTimeout(() => {
+    //   this.setState({
+    //     greeting: 'Hello again!!!'
+    //   })
+    // }, 3000)
     setTimeout(() => {
       this.setState({
-        greeting: 'Hello again!'
+        movies: [
+          {
+            title: "Trainspotting",
+            poster: "https://secure.i.telegraph.co.uk/multimedia/archive/02809/TRAINSPOTTING_2809300b.jpg"
+          },
+          ...this.state.movies
+        ]
       })
-    }, 3000)
+    },2000)
   }
 
   render(){
@@ -70,7 +82,7 @@ class App extends Component{
     return (
       <div className="App">
         {this.state.greeting}
-        {movies.map((movie, index) => {
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
