18621a8c55f56134d4db59c8b9e7c4d0846f923f
ahnoldmoon <munnt72@hotmail.com>
2019-05-30 07:06:10 +0900

    #6-1. Ajax in React : Complete

--- src/App.js
+++ src/App.js
@@ -45,32 +45,33 @@ class App extends Component{
     //     greeting: 'Hello again!!!'
     //   })
     // }, 3000)
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrix",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
-          },
-          {
-            title: "Full Metal Jacket",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
-          },
-          {
-            title: "Trainspotting",
-            poster: "https://secure.i.telegraph.co.uk/multimedia/archive/02809/TRAINSPOTTING_2809300b.jpg"
-          }
-        ]
-      })
-    },2000)
+    // setTimeout(() => {
+    //   this.setState({
+    //     movies: [
+    //       {
+    //         title: "Matrix",
+    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c1/The_Matrix_Poster.jpg/220px-The_Matrix_Poster.jpg"
+    //       },
+    //       {
+    //         title: "Full Metal Jacket",
+    //         poster: "https://images-na.ssl-images-amazon.com/images/I/91A%2B9lmEcxL._SX342_.jpg"
+    //       },
+    //       {
+    //         title: "Oldboy",
+    //         poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/67/Oldboykoreanposter.jpg/220px-Oldboykoreanposter.jpg"
+    //       },
+    //       {
+    //         title: "Star Wars",
+    //         poster: "https://i.ytimg.com/vi/ngElkyQ6Rhs/hqdefault.jpg"
+    //       },
+    //       {
+    //         title: "Trainspotting",
+    //         poster: "https://secure.i.telegraph.co.uk/multimedia/archive/02809/TRAINSPOTTING_2809300b.jpg"
+    //       }
+    //     ]
+    //   })
+    // },2000)
+    console.log(fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating'))
   }
 
   _renderMovies = () => {
