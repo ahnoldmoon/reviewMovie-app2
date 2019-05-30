985400d444e6295993386196c94e85732c980653
ahnoldmoon <munnt72@hotmail.com>
2019-05-31 07:26:25 +0900

    #6-3. Async Await in React : Complete

--- src/App.js
+++ src/App.js
@@ -8,17 +8,29 @@ class App extends Component{
   }
 
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(munnt => munnt.json())
-    .then(json => console.log(json))
-    .catch(err => console(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-      const movies = this.state.movies.map((movie, index) => {
-        return <Movie title={movie.title} poster={movie.poster} key={index} />
+      const movies = this.state.movies.map(movie => {
+        console.log(movie)
+        return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
       })
       return movies
+  }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(munnt => munnt.json())
+    .then(json => json.data.movies)
+    .catch(err => console(err))
   }
 
   render(){
