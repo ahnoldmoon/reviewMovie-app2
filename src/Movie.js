6c875fdffe112f5424ba8428ee7edf3a71a84094
ahnoldmoon <munnt72@hotmail.com>
2019-06-06 11:41:16 +0900

    #7-1. Updating Movie Component : Complete

--- src/Movie.js
+++ src/Movie.js
@@ -3,62 +3,51 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-// class Movie extends Component{
-
-//     static propTypes = {
-//         title: PropTypes.string.isRequired,
-//         poster: PropTypes.string.isRequired
-//     }
-
-//     render(){
-//         // console.log(this.props.title);
-//         // console.log(this.props);
-//         return(
-//             <div>
-//                 <MoviePoster poster={this.props.poster} />
-//                 <h1>{this.props.title}</h1>
-//             </div>
-//         )
-//     }
-// }
-
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
     return(
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <p className="Movie__Synopsis">>
+                    {synopsis}
+                </p>
+            </div>
         </div>
+    )
+}
+
+function MoviePoster({poster, alt}){
+    return(
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
+}
+
+function MovieGenre({genre}){
+    return (
+        <span className="Movie__Genre">{genre} </span>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
-}
-
-// class MoviePoster extends Component{
-
-//     static propTypes = {
-//         poster: PropTypes.string.isRequired
-//     }
-
-//     render(){
-//         console.log(this.props);
-//         return(
-//             // <img src="https://i.pinimg.com/236x/70/91/19/709119861d5e6870f47d552d60365025--hangers-hunting.jpg"/>
-//             <img src={this.props.poster} />
-//         )
-//     }
-// }
-
-function MoviePoster({poster}){
-    return(
-        <img src={poster} alt="Movie Poster" />
-    )
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 
 export default Movie;
